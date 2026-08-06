0334bb6ac60d9195cf503bd367c7293d6aed1726
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-08-07 00:41:22 +0530

    first commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -19,7 +19,6 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-@CrossOrigin("*")
 public class AuthController {
 
 	private final AuthService authService;
